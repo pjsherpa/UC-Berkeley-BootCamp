e5587979e9df7a26c0e7c099b489573f0af61e3f
Phinjock Sherpa <phinjock@gmail.com>
2022-07-11 23:28:02 -0700

    week5 day 2

--- 03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
+++ 03-JavaScript/03-Algorithms/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,9 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function (num) {
+  for (var i = 1; i < 11; i++) {
+    console.log(i);
+  }
+};
+logNums();
--- 05-Third-Party-APIs/03-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ 05-Third-Party-APIs/03-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,18 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function (arr) {
+  for (var i = 0; i < arr; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      console.log("fizzBuzz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+fizzBuzz(16);
