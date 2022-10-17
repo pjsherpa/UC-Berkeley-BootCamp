2250e3f8b419947d9267495d5945f64064413a15
Phinjock Sherpa <phinjock@gmail.com>
2022-10-16 18:51:44 -0700

    week 18 day 2

--- 18-NoSQL/01-Activities/18-Stu_Subdocuments/Unsolved/models/Library.js
+++ 18-NoSQL/01-Activities/18-Stu_Subdocuments/Unsolved/models/Library.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose');
 
 // TODO: Define a new schema named `bookSchema` for the subdocument
+const bookSchema= new mongoose.Schema({
+  title:String,
+  price:Number
+})
 
 const librarySchema = new mongoose.Schema({
   name: { type: String, required: true },
   //
   // TODO: Add the `books` subdocument to the parent document as an array
-  //
+  books:[bookSchema],
   lastAccessed: { type: Date, default: Date.now },
 });
 
 // TODO: Create a model named `Library`
+const Library=mongoose.model('library', librarySchema);
 
 // TODO: Create a new instance of the model including the subdocuments
+
+const bookData=[
+  {title:'Ann',price:50},
+  {title:'Jim',price:60},
+];
+
+Library.create(
+  {name:'new',books:bookData},
+  (err,data)=>{
+    if(err){
+      console.log(err);
+    }
+    console.log(data);
+  }
+)
+
 
 module.exports = Library;
